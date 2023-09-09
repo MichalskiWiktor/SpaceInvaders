93a17f8682c805c6a8b64f914a01f6bdfc5f8378
MichalskiWiktor <wiktor.kasa1@gmail.com>
2023-09-09 13:53:21 +0200

    reorganized classes and functions and deleted laser

--- Main.py
+++ Main.py
@@ -1,123 +1,103 @@
 import pygame
 import sys
+# Lasers
+BLUE_LASER = pygame.image.load("assets\\pixel_laser_blue.png")
+GREEN_LASER = pygame.image.load("assets\\pixel_laser_green.png")
+RED_LASER = pygame.image.load("assets\\pixel_laser_red.png")
+YELLOW_LASER = pygame.image.load("assets\\pixel_laser_yellow.png")
+# Ships
+BLUE_SHIP = pygame.image.load("assets\\pixel_ship_blue_small.png")
+GREEN_SHIP = pygame.image.load("assets\\pixel_ship_green_small.png")
+RED_SHIP = pygame.image.load("assets\\pixel_ship_red_small.png")
+YELLOW_SHIP = pygame.image.load("assets\\pixel_ship_yellow.png")
 
 # Game Basic Setting
-screen = pygame.display.set_mode((610, 600))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Space Invaders")
 bg_image = pygame.image.load("assets\\background-black.png")
-bg_image = pygame.transform.scale(bg_image, (610, 600))
+bg_image = pygame.transform.scale(bg_image, (600, 600))
 clock = pygame.time.Clock()
 pygame.init()
-#Klasy: Barier
+
+class Ship():
+    def __init__(self, position_x, position_y, width, height, vel):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.width = width
+        self.height = height
+        self.vel = vel
+        self.ship_image = None
+        self.laser_image = None
+        self.laser = None
+    
+    def draw(self, surface):
+        surface.blit(self.ship_image, (self.position_x, self.position_y))
+
+    def get_width(self):
+        return self.ship_image.get_width()
+    
+    def get_height(self):
+        return self.ship_image.get_height()
 
 
-class Player(object):
-    image = pygame.image.load("assets\\pixel_ship_yellow.png").convert_alpha()
-    size = 50
-    laser = None
+class Player(Ship):
 
-    def __init__(self):
-        self.position = 0
-        self.image = pygame.transform.scale(self.image, (Player.size, Player.size))
+    def __init__(self, position_x, position_y, width, height, vel):
+        super().__init__(position_x, position_y, width, height, vel)
+        self.ship_image = pygame.image.load("assets\\pixel_ship_yellow.png").convert_alpha()
+        self.ship_image = pygame.transform.scale(self.ship_image, (self.width, self.height))
 
     def handle_keys(self):
         key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] or key[pygame.K_a] and self.position>0:
-           self.position-=5
-        if key[pygame.K_RIGHT] or key[pygame.K_d] and self.position<550:
-           self.position+=5
-        if key[pygame.K_SPACE]:
-            if Player.laser is None:
-                Player.laser = Laser(self.position, 450)
-
-    def draw(self, surface):
-       surface.blit(self.image, (self.position, 500))
+        if key[pygame.K_LEFT] or key[pygame.K_a] and self.position_x - self.vel > 0:
+           self.position_x-=self.vel
+        if key[pygame.K_RIGHT] or key[pygame.K_d] and self.position_x + self.width + self.vel < 600:
+           self.position_x+=self.vel
 
 
-class Enemy:
-    speed = 10
-    size = 50
-    direction = "right"
-    standard_position_y = 0
+class Enemy(Ship):
+    group_colors_map = {
+                        "red": (RED_SHIP, RED_LASER),
+                        "green": (GREEN_SHIP, GREEN_LASER),
+                        "blue": (BLUE_SHIP, BLUE_LASER)
+                        }
     
-    def __init__(self, position_x, position_y, points, is_alive, image):
-        self.position_x = position_x
-        self.position_y = position_y
+    def __init__(self, position_x, position_y, width, height, vel, points, color):
+        super().__init__(position_x, position_y, width, height, vel)
         self.points = points
-        self.is_alive = is_alive
-        self.image = image
-        self.image = pygame.transform.scale(self.image, (Enemy.size, Enemy.size))
+        self.ship_image, self.laser_image = self.group_colors_map[color]
+        self.ship_image = pygame.transform.scale(self.ship_image, (self.width, self.height))
+        self.direction = "right"
 
     def change_position(self):
-        if Enemy.direction=="left":
-            self.position_x -= Enemy.speed
+        if self.direction=="left":
+            self.position_x -= self.vel
         else:
-            self.position_x += Enemy.speed
-    
-    def get_position_y(self):
-        return self.position_y + Enemy.standard_position_y
-    
-    @classmethod
-    def change_level(cls, side):
-        if side == "left":
-            Enemy.direction="right" 
-            Enemy.standard_position_y += 25
+            self.position_x += self.vel
+
+    def change_level(self):
+        if self.direction == "left":
+            self.position_y += 25
         else:
-            Enemy.direction="left"
-            Enemy.standard_position_y += 25
-
-    def draw(self, surface):
-       surface.blit(self.image, (self.position_x, (self.position_y + Enemy.standard_position_y)))
-
-
-class Laser:
+            self.position_y += 25
     
-    def __init__(self, position_x, position_y):
-        self.is_alive = True
-        self.image = pygame.image.load("assets\\pixel_laser_yellow.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.position_x = position_x
-        self.position_y = position_y
-    #https://www.techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/pygame-collision
-    def change_position(self):
-        for enemies in enemy_rows:
-            for enemy in enemies:
-                if enemy.get_position_y == self.position_y and enemy.position_x == self.position_x:
-                    enemy.is_alive = False
-                    self.is_alive = False
-                    enemies.pop(enemies.index(enemy))
-                    print("dead")
-                    pass
+    def change_direction(self, direction):
+        self.direction = direction
 
-        if self.position_y <= 0 or self.is_alive == False:
-            Player.laser = None
-        else:
-            self.position_y -= 20
-    
-    def draw(self, surface):
-       surface.blit(self.image, (self.position_x, self.position_y))
 
-# Enemy Creation Section
-imgs = [pygame.image.load("assets\\pixel_ship_red_small.png").convert_alpha(), 
-        pygame.image.load("assets\\pixel_ship_green_small.png").convert_alpha(),
-        pygame.image.load("assets\\pixel_ship_blue_small.png").convert_alpha(),
-        pygame.image.load("assets\\pixel_ship_yellow.png").convert_alpha()]
-
-def get_enemy(position_x, position_y, points, img):
-    return Enemy(position_x, position_y, points, True, img)
+def get_enemy(position_x, position_y, width, height, vel, points, color):
+    return Enemy(position_x, position_y, width, height, vel, points, color)
 
 enemy_rows = []
-enemy_rows.append([get_enemy(i, 50, 75, imgs[0]) for i in range(0, 400, 50)])
-enemy_rows.append([get_enemy(i, 100, 50, imgs[1]) for i in range(0, 400, 50)])
-enemy_rows.append([get_enemy(i, 150, 50, imgs[1]) for i in range(0, 400, 50)])
-enemy_rows.append([get_enemy(i, 200, 25, imgs[2]) for i in range(0, 400, 50)])
-enemy_rows.append([get_enemy(i, 250, 25, imgs[2]) for i in range(0, 400, 50)])
+enemy_rows.append([get_enemy(i, 50, 50, 50, 15, 75, "red") for i in range(0, 400, 50)])
+enemy_rows.append([get_enemy(i, 100, 50, 50, 15, 50, "green") for i in range(0, 400, 50)])
+enemy_rows.append([get_enemy(i, 150, 50, 50, 15, 50, "green") for i in range(0, 400, 50)])
+enemy_rows.append([get_enemy(i, 200, 50, 50, 15, 25, "blue") for i in range(0, 400, 50)])
+enemy_rows.append([get_enemy(i, 250, 50, 50, 15, 25, "blue") for i in range(0, 400, 50)])
 
-player = Player()
+player = Player(300, 500, 50, 50, 5)
 MOVE_PLAYER_EVENT = pygame.USEREVENT + 1
-MOVE_PLAYER_LASER_EVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(MOVE_PLAYER_EVENT, 700)
-pygame.time.set_timer(MOVE_PLAYER_LASER_EVENT, 100)
 
 # Game Loop
 while True:
@@ -128,28 +108,25 @@ while True:
         elif event.type == MOVE_PLAYER_EVENT:  # Player Event
             # update direction of movement
             for enemies in enemy_rows:
-                if enemies[-1].position_x >= 560 and Enemy.direction == "right":
-                    Enemy.change_level("right")
-                elif enemies[0].position_x <= 0 and Enemy.direction == "left":
-                    Enemy.change_level("left")
+                if enemies[-1].position_x + enemies[-1].width + enemies[-1].vel>= 600 and enemies[-1].direction == "right":
+                    for enemy in enemies:
+                        enemy.change_direction("left")
+                        enemy.change_level()
+                elif enemies[0].position_x <= 0 and enemies[0].direction == "left":
+                    for enemy in enemies:
+                        enemy.change_direction("right")
+                        enemy.change_level()
 
             # move all enemies in the same direction
             for enemies in enemy_rows:
                 for enemy in enemies:
                     enemy.change_position()
-        # move player laser
-        elif event.type == MOVE_PLAYER_LASER_EVENT:
-            if Player.laser is not None and Player.laser.is_alive == True:
-                Player.laser.change_position()
 
     screen.blit(bg_image, (0, 0))
     for enemies in enemy_rows:
         for enemy in enemies:
-            if enemy.is_alive == True:
-                enemy.draw(screen)
+            enemy.draw(screen)
     player.draw(screen)
-    if Player.laser is not None and Player.laser.is_alive == True:
-        Player.laser.draw(screen)
     player.handle_keys()
     
     pygame.display.update()
