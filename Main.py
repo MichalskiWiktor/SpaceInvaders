5ea8a4d8be4baebd464f409d2a0d32b0ec2019ac
MichalskiWiktor <wiktor.kasa1@gmail.com>
2023-09-10 12:13:30 +0200

    added enemy laser

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 # Lasers
 BLUE_LASER = pygame.image.load("assets\\pixel_laser_blue.png")
 GREEN_LASER = pygame.image.load("assets\\pixel_laser_green.png")
@@ -20,7 +21,7 @@ clock = pygame.time.Clock()
 pygame.init()
 
 class Ship():
-    def __init__(self, position_x, position_y, width, height, vel):
+    def __init__(self, position_x, position_y, width, height, vel, laser_vel):
         self.position_x = position_x
         self.position_y = position_y
         self.width = width
@@ -29,15 +30,15 @@ class Ship():
         self.ship_image = None
         self.laser_image = None
         self.laser = None
+        self.laser_vel = laser_vel
     
     def draw(self, surface):
         surface.blit(self.ship_image, (self.position_x, self.position_y))
         if self.laser is not None:
             self.laser.draw(surface)
-
-    def shoot(self):
+    def shoot(self, spawn):
         if self.laser is None:
-            self.laser = Laser(self.position_x, self.position_y - 50, self.laser_image)
+            self.laser = Laser(self.position_x, self.position_y + spawn, self.laser_image, self.laser_vel)
     def get_width(self):
         return self.ship_image.get_width()
     
@@ -47,10 +48,11 @@ class Ship():
 
 class Player(Ship):
     def __init__(self, position_x, position_y, width, height, vel):
-        super().__init__(position_x, position_y, width, height, vel)
+        super().__init__(position_x, position_y, width, height, vel, -20)
         self.ship_image = pygame.transform.scale(YELLOW_SHIP, (self.width, self.height))
         self.laser_image = pygame.transform.scale(YELLOW_LASER, (self.width, self.height))
         self.mask = pygame.mask.from_surface(self.ship_image) 
+        self.lifes = 3
 
     def handle_keys(self):
         key = pygame.key.get_pressed()
@@ -59,7 +61,7 @@ class Player(Ship):
         if key[pygame.K_RIGHT] or key[pygame.K_d] and self.position_x + self.width + self.vel < 600:
            self.position_x+=self.vel
         if key[pygame.K_SPACE]:
-           self.shoot()
+           self.shoot(-50)
 
 
 class Enemy(Ship):
@@ -71,7 +73,7 @@ class Enemy(Ship):
     }
     
     def __init__(self, position_x, position_y, width, height, vel, points, color):
-        super().__init__(position_x, position_y, width, height, vel)
+        super().__init__(position_x, position_y, width, height, vel, 20)
         self.points = points
         self.ship_image, self.laser_image = self.group_colors_map[color]
         self.ship_image = pygame.transform.scale(self.ship_image, (self.width, self.height))
@@ -93,21 +95,18 @@ class Enemy(Ship):
 
 
 class Laser:
-    def __init__(self, position_x, position_y, image):
+    def __init__(self, position_x, position_y, image, vel):
         self.image = pygame.transform.scale(image, (50, 50))
         self.position_x = position_x
         self.position_y = position_y
-        self.vel = 20
         self.mask = pygame.mask.from_surface(self.image) 
+        self.vel = vel
 
     def draw(self, surface):
         surface.blit(self.image, (self.position_x, self.position_y))
 
     def move(self):
-        self.position_y -= self.vel
-
-    def out_of_scrren(self):
-        player.laser = None
+        self.position_y += self.vel
 
     def collision(self, obj):
         return collide(obj, self)
@@ -128,10 +127,15 @@ enemy_rows.append([get_enemy(i, 200, 50, 50, 15, 25, "blue") for i in range(0, 4
 enemy_rows.append([get_enemy(i, 250, 50, 50, 15, 25, "blue") for i in range(0, 400, 50)])
 
 player = Player(300, 500, 50, 50, 5)
-MOVE_PLAYER_EVENT = pygame.USEREVENT + 1
+
+MOVE_ENEMY_EVENT = pygame.USEREVENT + 1
 MOVE_PLAYER_LASER_EVENT = pygame.USEREVENT + 2
-pygame.time.set_timer(MOVE_PLAYER_EVENT, 700)
-pygame.time.set_timer(MOVE_PLAYER_LASER_EVENT, 75)
+MOVE_ENEMY_LASER_EVENT = pygame.USEREVENT + 3
+CREATE_ENEMY_LASER_EVENT = pygame.USEREVENT + 4
+pygame.time.set_timer(MOVE_ENEMY_EVENT, 700)
+pygame.time.set_timer(MOVE_PLAYER_LASER_EVENT, 60)
+pygame.time.set_timer(MOVE_ENEMY_LASER_EVENT, 100)
+pygame.time.set_timer(CREATE_ENEMY_LASER_EVENT, 2000)
 
 # Game Loop
 while True:
@@ -139,7 +143,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == MOVE_PLAYER_EVENT:  # Player Event
+        elif event.type == CREATE_ENEMY_LASER_EVENT:
+            random_row = random.choice(enemy_rows)
+            enemy = random.choice(random_row)
+            enemy.shoot(50)
+        elif event.type == MOVE_ENEMY_EVENT:  # Player Event
             # update direction of movement
             # Double list comprehension to see 
             # if any enemy touches the right side of window and 
@@ -164,23 +172,36 @@ while True:
             for enemies in enemy_rows:
                 for enemy in enemies:
                     enemy.change_position()
+        elif event.type == MOVE_ENEMY_LASER_EVENT:
+            for enemies in enemy_rows:
+                for enemy in enemies:
+                    if enemy.laser is not None: # Does Laser exist
+                        if enemy.laser.position_y + enemy.laser.vel <= 600: # If the Laser is out of the Map
+                            if enemy.laser.collision(player) == True: # Does the laser collide with any enemy
+                                player.lifes-=1
+                                print("atak")
+                                enemy.laser = None
+                            if enemy.laser is not None:
+                                enemy.laser.move()
+                        else:
+                            enemy.laser = None
         elif event.type == MOVE_PLAYER_LASER_EVENT:
-            if player.laser is not None: # Does Laser exist
-                if player.laser.position_y - player.laser.vel >= 0: # If the Laser is out of the Map
-                    loop_escape = False
-                    for enemies in enemy_rows:
-                        for enemy in enemies:
-                            if player.laser.collision(enemy) == True: # Does the laser collide with any enemy
-                                enemies.pop(enemies.index(enemy))
-                                player.laser = None
-                                loop_escape = True
+                if player.laser is not None: # Does Laser exist
+                    if player.laser.position_y + player.laser.vel >= 0: # If the Laser is out of the Map
+                        loop_escape = False
+                        for enemies in enemy_rows:
+                            for enemy in enemies:
+                                if player.laser.collision(enemy) == True: # Does the laser collide with any enemy
+                                    enemies.pop(enemies.index(enemy))
+                                    player.laser = None
+                                    loop_escape = True
+                                    break
+                            if loop_escape:
                                 break
-                        if loop_escape:
-                            break
-                    if player.laser is not None:
-                        player.laser.move()
-                else:
-                    player.laser.out_of_scrren()
+                        if player.laser is not None:
+                            player.laser.move()
+                    else:
+                        player.laser = None
 
 
     screen.blit(bg_image, (0, 0))
