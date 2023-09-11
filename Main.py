ce8059354d3ce1f1930ec27d67bafd695a3491e7
MichalskiWiktor <wiktor.kasa1@gmail.com>
2023-09-11 14:11:49 +0200

    added win and lose and list comprehension

--- Main.py
+++ Main.py
@@ -141,91 +141,93 @@ pygame.time.set_timer(MOVE_ENEMY_LASER_EVENT, 100)
 pygame.time.set_timer(CREATE_ENEMY_LASER_EVENT, 2000)
 
 # Game Loop
+run = True
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == CREATE_ENEMY_LASER_EVENT:
-            random_row = random.choice(enemy_rows)
-            if len(random_row) != 0:
-                enemy = random.choice(random_row)
-                enemy.shoot(50)
-        elif event.type == MOVE_ENEMY_EVENT:  # Player Event
+    if run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == CREATE_ENEMY_LASER_EVENT:
+                try:
+                    random_row = random.choice(enemy_rows)
+                    if len(random_row) != 0:
+                        enemy = random.choice(random_row)
+                        enemy.shoot(50)
+                except IndexError:
+                    end_game("You Won !!!")
+            elif event.type == MOVE_ENEMY_EVENT:  # Player Event
+                # Delete an enemy row if it is empty
+                [enemy_rows.pop(enemy_rows.index(enemies)) 
+                 for enemies in enemy_rows if len(enemies) == 0]
 
-            # Check if there are any enemies 
-            if len(enemy_rows) == 0:
-                print("win")
-            else:
-                for enemies in enemy_rows:
-                    if len(enemies) == 0:
-                        enemy_rows.pop(enemy_rows.index(enemies))
+                # update direction of movement
+                # Double list comprehension to see 
+                # if any enemy touches the right side of window and 
+                # if yes then we change level and directrion of movment
+                if any(Enemy.group_direction == "right" and 
+                    enemy.position_x + enemy.width + enemy.vel>= 600 
+                    for enemies in enemy_rows for enemy in enemies):
+                        Enemy.group_direction = "left"
+                        change_enemies_level()
+                elif any(Enemy.group_direction == "left" and 
+                    enemy.position_x <= 0 
+                    for enemies in enemy_rows for enemy in enemies):
+                        Enemy.group_direction = "right"
+                        change_enemies_level()
+                
+                def change_enemies_level():
+                    [enemy.change_level() for enemies in enemy_rows for enemy in enemies]
 
-            # update direction of movement
-            # Double list comprehension to see 
-            # if any enemy touches the right side of window and 
-            # if yes then we change level and directrion of movment
-            if any(Enemy.group_direction == "right" and 
-                   enemy.position_x + enemy.width + enemy.vel>= 600 
-                   for enemies in enemy_rows for enemy in enemies):
-                Enemy.group_direction = "left"
-                change_enemies_level()
-            elif any(Enemy.group_direction == "left" and 
-                   enemy.position_x <= 0 
-                   for enemies in enemy_rows for enemy in enemies):
-               Enemy.group_direction = "right"
-               change_enemies_level()
-            
-            def change_enemies_level():
+                # move all enemies in the same direction
+                [enemy.change_position() for enemies in enemy_rows for enemy in enemies]
+
+            elif event.type == MOVE_ENEMY_LASER_EVENT:
                 for enemies in enemy_rows:
                     for enemy in enemies:
-                        enemy.change_level()
-
-            # move all enemies in the same direction
-            for enemies in enemy_rows:
-                for enemy in enemies:
-                    enemy.change_position()
-
-        elif event.type == MOVE_ENEMY_LASER_EVENT:
-            for enemies in enemy_rows:
-                for enemy in enemies:
-                    if enemy.laser is not None: # Does Laser exist
-                        if enemy.laser.position_y + enemy.laser.vel <= 600: # If the Laser is out of the Map
-                            if enemy.laser.collision(player) == True: # Does the laser collide with any enemy
-                                player.lives-=1
-                                print("atak")
+                        if enemy.laser is not None: # Does Laser exist
+                            if enemy.laser.position_y + enemy.laser.vel <= 600: # If the Laser is out of the Map
+                                if enemy.laser.collision(player) == True: # Does the laser collide with player
+                                    player.lives-=1
+                                    enemy.laser = None
+                                if enemy.laser is not None:
+                                    enemy.laser.move()
+                            else:
                                 enemy.laser = None
-                            if enemy.laser is not None:
-                                enemy.laser.move()
-                        else:
-                            enemy.laser = None
-        elif event.type == MOVE_PLAYER_LASER_EVENT:
-                if player.laser is not None: # Does Laser exist
-                    if player.laser.position_y + player.laser.vel >= 0: # If the Laser is out of the Map
-                        loop_escape = False
-                        for enemies in enemy_rows:
-                            for enemy in enemies:
-                                if player.laser.collision(enemy) == True: # Does the laser collide with any enemy
-                                    player.score += enemy.points
-                                    enemies.pop(enemies.index(enemy))
-                                    player.laser = None
-                                    loop_escape = True
+            elif event.type == MOVE_PLAYER_LASER_EVENT:
+                    if player.laser is not None: # Does Laser exist
+                        if player.laser.position_y + player.laser.vel >= 0: # If the Laser is out of the Map
+                            loop_escape = False
+                            for enemies in enemy_rows:
+                                for enemy in enemies:
+                                    if player.laser.collision(enemy) == True: # Does the laser collide with any enemy
+                                        player.score += enemy.points
+                                        enemies.pop(enemies.index(enemy))
+                                        player.laser = None
+                                        loop_escape = True
+                                        break
+                                if loop_escape:
                                     break
-                            if loop_escape:
-                                break
-                        if player.laser is not None:
-                            player.laser.move()
-                    else:
-                        player.laser = None
+                            if player.laser is not None:
+                                player.laser.move()
+                        else:
+                            player.laser = None
 
-    screen.blit(bg_image, (0, 0))
+    screen.blit(bg_image, (0, 0))  
+    def end_game(result):
+        end_label = main_font.render(result, 1, (255, 255, 255))
+        screen.blit(end_label, (230,260))
+        pygame.display.update()
+        pygame.time.delay(5000)
+        pygame.quit()
+        sys.exit()  
+    if player.lives == 0:
+        end_game("You lost !!!")
     lives_label = main_font.render(f"Lives: {player.lives}", 1, (255, 255, 255))
     score_label = main_font.render(f"Score: {player.score}", 1, (255, 255, 255))
     screen.blit(lives_label, (500, 10))
     screen.blit(score_label, (11, 10))
-    for enemies in enemy_rows:
-        for enemy in enemies:
-            enemy.draw(screen)
+    [enemy.draw(screen) for enemies in enemy_rows for enemy in enemies]
     player.draw(screen)
     player.handle_keys()
     
